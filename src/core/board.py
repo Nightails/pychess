ed8931d28472102b0654617b9f707078956fe93e
Alan Night <a.nightails@gmail.com>
2025-07-26 11:12:30 -0500

    Add pieces to board with position and direction

---
+++ src/core/board.py
@@ -0,0 +1,52 @@
+from pygame.math import Vector2
+from core.piece import Color, Pawn, Rook, Knight, Bishop, Queen, King
+
+
+class Board:
+    def __init__(self):
+        self.pieces: list = []  # track pieces currently on board
+        self.board: dict = {}  # track currently occupied spaces
+
+        # spawning pieces
+        self.pieces.extend(init_pieces(Color.WHITE))
+        self.pieces.extend(init_pieces(Color.BLACK))
+
+        # board layout
+        self.board = init_board_layout(self.pieces)
+
+
+def init_pieces(color: Color) -> list:
+    pieces = []
+
+    # White facing up and Black facing down
+    dir = Vector2(0, 1) if color is Color.WHITE else Vector2(0, -1)
+
+    # pawns
+    for x in range(1, 9):
+        y = 2 if color is Color.WHITE else 7
+        pos = Vector2(x, y)
+        pieces.append(Pawn(color, pos, dir))
+
+    y = 1 if color is Color.WHITE else 8
+    # rooks
+    pieces.append(Rook(color, Vector2(1, y), dir))
+    pieces.append(Rook(color, Vector2(8, y), dir))
+    # knights
+    pieces.append(Knight(color, Vector2(2, y), dir))
+    pieces.append(Knight(color, Vector2(7, y), dir))
+    # bishops
+    pieces.append(Bishop(color, Vector2(3, y), dir))
+    pieces.append(Bishop(color, Vector2(6, y), dir))
+    # queen
+    pieces.append(Queen(color, Vector2(4, y), dir))
+    # king
+    pieces.append(King(color, Vector2(5, y), dir))
+
+    return pieces
+
+
+def init_board_layout(pieces: list) -> dict:
+    board = {}
+    for piece in pieces:
+        board[piece.position.x, piece.position.y] = piece
+    return board
--- src/core/piece.py
+++ src/core/piece.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from vectormath import Vector2
+from pygame.math import Vector2
 
 
 class Color(Enum):
@@ -17,40 +17,40 @@ class Name(Enum):
 
 
 class Piece:
-    def __init__(self, name: Name, color: Color, position: Vector2):
+    def __init__(self, name: Name, color: Color, position: Vector2, direction: Vector2):
         self.name = name
         self.color = color
         self.position = position
 
     def __repr__(self):
-        return f"{self.color} {self.name} at {self.position}"
+        return f"{self.color.value} {self.name.value} at {self.position}"
 
 
 class Pawn(Piece):
-    def __init__(self, color: Color, position: Vector2):
-        super().__init__(Name.PAWN, color, position)
+    def __init__(self, color: Color, position: Vector2, direction: Vector2):
+        super().__init__(Name.PAWN, color, position, direction)
 
 
 class Bishop(Piece):
-    def __init__(self, color: Color, position: Vector2):
-        super().__init__(Name.BISHOP, color, position)
+    def __init__(self, color: Color, position: Vector2, direction: Vector2):
+        super().__init__(Name.BISHOP, color, position, direction)
 
 
 class Knight(Piece):
-    def __init__(self, color: Color, position: Vector2):
-        super().__init__(Name.KNIGHT, color, position)
+    def __init__(self, color: Color, position: Vector2, direction: Vector2):
+        super().__init__(Name.KNIGHT, color, position, direction)
 
 
 class Rook(Piece):
-    def __init__(self, color: Color, position: Vector2):
-        super().__init__(Name.ROOK, color, position)
+    def __init__(self, color: Color, position: Vector2, direction: Vector2):
+        super().__init__(Name.ROOK, color, position, direction)
 
 
 class Queen(Piece):
-    def __init__(self, color: Color, position: Vector2):
-        super().__init__(Name.QUEEN, color, position)
+    def __init__(self, color: Color, position: Vector2, direction: Vector2):
+        super().__init__(Name.QUEEN, color, position, direction)
 
 
 class King(Piece):
-    def __init__(self, color: Color, position: Vector2):
-        super().__init__(Name.KING, color, position)
+    def __init__(self, color: Color, position: Vector2, direction: Vector2):
+        super().__init__(Name.KING, color, position, direction)
