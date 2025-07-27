4b08f17449bf504bd7873a643e26435e7216ee9f
Alan Night <a.nightails@gmail.com>
2025-07-27 01:17:49 -0500

    Organize similar panels into one file

--- src/pychess_app.py
+++ src/pychess_app.py
@@ -5,10 +5,7 @@ from textual.containers import Container, Horizontal
 from src.core.gamestate import GameState
 from src.core.board import Board
 from src.ui.board_panel import BoardPanel
-from src.ui.info_panel import InfoPanel
-from src.ui.log_panel import LogPanel
-from src.ui.cmd_panel import CMDPanel
-from src.ui.panel import StatusPanel
+from src.ui.panel import StatusPanel, LogPanel, InfoPanel, CMDPanel
 
 
 class PyChess(App):
--- src/ui/panel.py
+++ src/ui/panel.py
@@ -1,4 +1,5 @@
-from textual.widgets import Static
+from textual.widgets import Static, Input
+
 
 class StatusPanel(Static):
     def __init__(self, turn: str):
@@ -8,3 +9,39 @@ class StatusPanel(Static):
         self.styles.border = ("round", "white")
         self.styles.content_align = ("center", "middle")
         self.update(turn)
+
+
+class InfoPanel(Static):
+    def __init__(self):
+        super().__init__()
+        self.border_title = "Info"
+        self.styles.border = ("round", "white")
+        self.styles.width = "1fr"
+
+        self.update("Info about player and turn status go here")
+
+
+class LogPanel(Static):
+    contents = ["Previous moves go here"]
+
+    def __init__(self):
+        super().__init__()
+        self.border_title = "Log"
+        self.styles.border = ("round", "white")
+        self.styles.width = "1fr"
+
+        self.update(self.contents[0])
+
+    def update_log(self, content):
+        self.contents.append(content)
+        self.update("\n".join(self.contents))
+
+
+class CMDPanel(Input):
+    def __init__(self):
+        super().__init__()
+        self.border_title = "Command"
+        self.styles.border = ("round", "white")
+        self.styles.background = "black"
+
+        self.placeholder = "Type move here"
